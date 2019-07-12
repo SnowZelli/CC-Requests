8253c132fca47d2712cf189a6fe03c04f402fdba
nrbozzelli <nrbozzelli@gmail.com>
2019-07-12 16:17:35 -0600

    Completed steps 34-43.

--- public/helpers.js
+++ public/helpers.js
@@ -1,9 +1,9 @@
-const createVenueHTML = (name, location, iconSource) => {
+const createVenueHTML = (name, location, state, postalCode, iconSource) => {
     return `<h2>${name}</h2>
     <img class="venueimage" src="${iconSource}"/>
     <h3>Address:</h3>
     <p>${location.address}</p>
-    <p>${location.city}</p>
+    <p>${location.city}, ${state}, ${postalCode}</p>
     <p>${location.country}</p>`;
   }
   
@@ -11,5 +11,6 @@ const createVenueHTML = (name, location, iconSource) => {
     return `<h2> High: ${currentDay.day.maxtemp_f}</h2>
       <h2> Low: ${currentDay.day.mintemp_f}</h2>
       <img src="https://${currentDay.day.condition.icon}" class="weathericon" />
-      <h2>${weekDays[(new Date(currentDay.date)).getDay()]}</h2>`;
+      <h2>${weekDays[(new Date(currentDay.date)).getDay()]}</h2>
+      <p>Humidity: ${currentDay.day.avghumidity}%</p>`;
   }
--- public/main.js
+++ public/main.js
@@ -12,19 +12,21 @@ const $input = $('#city');
 const $submit = $('#button');
 const $destination = $('#destination');
 const $container = $('.container');
-const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
+const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3")];
 const $weatherDivs = [$("#weather1"), $("#weather2"), $("#weather3"), $("#weather4")];
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 // Add AJAX functions here:
 const getVenues = async () => {
     const city = $input.val();
-    const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20190710`;
+    const urlToFetch = `${url}${city}&limit=100&client_id=${clientId}&client_secret=${clientSecret}&v=20190710`;
     try {
       const response = await fetch(urlToFetch);
+      //console.log(response);
       if(response.ok) {
         const jsonResponse = await response.json();
         const venues = jsonResponse.response.groups[0].items.map(item => item.venue);
+        //console.log(venues);
         return venues;
       }
     }
@@ -37,6 +39,7 @@ const getForecast = async () => {
   const urlToFetch = `${forecastUrl}${apiKey}&q=${$input.val()}&days=4&hour=11`;
   try {
     const response = await fetch(urlToFetch);
+    //console.log(response);
     if(response.ok) {
       const jsonResponse = await response.json();
       //console.log(jsonResponse);
@@ -51,11 +54,12 @@ const getForecast = async () => {
 
 // Render functions
 const renderVenues = (venues) => {
-  $venueDivs.forEach(($venue, index) => {
-    const venue = venues[index];
+  $venueDivs.forEach(($venue) => {
+    let randomNumber = Math.floor(Math.random() * 100);
+    const venue = venues[randomNumber];
     const venueIcon = venue.categories[0].icon;
     const venueImgSrc = `${venueIcon.prefix}bg_64${venueIcon.suffix}`;
-    let venueContent = createVenueHTML(venue.name, venue.location, venueImgSrc);
+    let venueContent = createVenueHTML(venue.name, venue.location, venue.location.state, venue.location.postalCode, venueImgSrc);
     $venue.append(venueContent);
   });
   $destination.append(`<h2>${venues[0].location.city}</h2>`);
@@ -63,10 +67,8 @@ const renderVenues = (venues) => {
 
 const renderForecast = (days) => {
   $weatherDivs.forEach(($day, index) => {
-    // Add your code here:
-
-
-    let weatherContent = '';
+    const currentDay = days[index];
+    let weatherContent = `${createWeatherHTML(currentDay)}`;
     $day.append(weatherContent);
   });
 }
@@ -81,6 +83,9 @@ const executeSearch = () => {
       return renderVenues(venues);
     });
   getForecast()
+    .then((days) => {
+      return renderForecast(days);
+    });
   return false;
 }
 
